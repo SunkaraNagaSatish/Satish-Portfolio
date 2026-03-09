94cf4b94624699deae819a792c82d9f5a3c5c8bf
NaGaSatisH <nagsatish7893@gmail.com>
2026-03-09 17:38:37 +0530

    Refactor Vite config for build and plugins
    Update Vite configuration to include rollup options and modify build output directory.

--- Satish-Portfolio/vite.config.js
+++ Satish-Portfolio/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './portfolio',
   base: '/Satish-Portfolio/',
+  plugins: [react()],
   build: {
-    outDir: '../dist'
-  },
-  plugins: [react()]
-});
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
+})
