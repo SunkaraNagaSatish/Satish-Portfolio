fad355212154a3ee621031661c43b1ca12902cff
NaGaSatisH <nagsatish7893@gmail.com>
2026-03-09 16:56:05 +0530

    Update vite.config.js

--- Satish-Portfolio/vite.config.js
+++ Satish-Portfolio/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Satish-Portfolio/', // Replace with your repository name
+  root: './portfolio',
+  base: '/Satish-Portfolio/',
+  build: {
+    outDir: '../dist'
+  },
   plugins: [react()]
 });
